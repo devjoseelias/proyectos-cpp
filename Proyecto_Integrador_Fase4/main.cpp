248b819a64c3840f29234d340e031941611d2b89
José Elías <devjoseelias@gmail.com>
2026-09-15 11:29:27 -0600

    agregué un else en caso de que un argumento no se reconozca

--- Proyecto_Integrador_Fase4/main.cpp
+++ Proyecto_Integrador_Fase4/main.cpp
@@ -96,5 +96,8 @@ int main(int argc, char* argv[]){
         std::cin >> calificacion;
         verificar_datos(nombre, registro, calificacion);
         db.registrar_alumno(std::move(nombre), std::move(registro), std::move(calificacion));
+    } else{
+        std::cout << "Comando no válido.\n";
+        pedir_ayuda();
     }
 }
